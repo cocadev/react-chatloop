ec9625ed2bd53f8e875413425193c69a5fab045c
kingofdevs <40970351+kingofdevs@users.noreply.github.com>
2020-06-01 17:12:48 +0800

    workplace

--- src/screens/section/workplace.js
+++ src/screens/section/workplace.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { WorkAtom, WorkHoverAtom } from '../../atoms/works';
 
 export default function WorkPlaceSection() {
+    const [i, setI ] = useState(1)
     return (
         <section className="team-bg" id="team">
         <div className="animation-circle-inverse"><i></i><i></i><i></i></div>
@@ -8,144 +10,18 @@ export default function WorkPlaceSection() {
           <div className="row">
             <div className="col-md-12 text-center">
               <div className="section-title">
-                <h2>Our Work Place </h2>
+                <h2>Our Work Place {i}</h2>
                 <div className="line"></div>
               </div>
             </div>
             <div className="col-md-12">
               <div className="team-slider owl-carousel owl-theme">
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/1.jpg" alt="1" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/2.jpg" alt="2" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/3.jpg" alt="3" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/2.jpg" alt="2" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/1.jpg" alt="1" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="item">
-                  <div className="team-box">
-                    <div className="team-under-box">
-                      <div className="team-under-box-button text-white"><i className="fa fa-plus" aria-hidden="true"></i></div><img className="img-fluid" src="images/2.jpg" alt="2" />
-                      <div className="team-overlay">
-                        <div className="social-icon">
-                          <ul>
-                            <li><i className="fa fa-facebook" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-google-plus" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-twitter" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-instagram" aria-hidden="true"></i></li>
-                            <li><i className="fa fa-rss" aria-hidden="true"></i></li>
-                          </ul>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                <WorkAtom i={1}/>  
+                <WorkAtom i={2}/>  
+                <WorkAtom i={3}/>  
               </div>
             </div>
-            <div className="col-md-12">
-              <div className="team-hover">
-                <div className="row">
-                  <div className="col-md-4">
-                    <div className="team-profile"><img className="img-fluid" src="images/1.jpg" alt="1" /></div>
-                  </div>
-                  <div className="col-md-8">
-                    <div className="d-flex-1">
-                      <div className="hover-text">
-                        <div className="team-close-btn">
-                          <i className="fa fa-times" aria-hidden="true"></i>
-                        </div>
-                        <h3>Mark Jecno </h3>
-                        <h4 className="m-0 text-muted">Devloper & Designer </h4>
-                        <h5>PHP, Laravel, Cake , _________, HTML5  </h5>
-                        <p>It is a long ___________ fact that a reader ____ be distracted by the ________ content of a page ____ looking at its layout. ___ point of using Lorem _____ is that it has _ more-or-less normal distribution of _______, as opposed to using '_______ here, content here', making __ look like readable English. ____ desktop publishing packages and ___ page editors now use _____ Ipsum as their default _____ text, </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <WorkHoverAtom i={i}/>
           </div>
         </div>
       </section>
